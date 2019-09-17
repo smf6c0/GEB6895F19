3d1a7c17431a697fa540a4f7c8ff43b1afbf441b
Shane Fenton <shane.fenton@gmail.com>
2019-09-17 19:10:28 -0400

    updated ex 4, 5

--- assignment_01/my_answers/example_04.R
+++ assignment_01/my_answers/example_04.R
@@ -1,14 +1,10 @@
 # Example 4 Write an R function to print all multiples of 5 between 1 and n (possibly including n).
 
-{
-  n<-as.integer(readline(prompt = "Enter a number: "))
-}
-
-nlist<-seq(1, n, by = 1)
-
-f<-function(nlist)
-{
-  temp <- nlist%%5
-  return(temp)
+multiple.5<-function(n) {
+  x<-(c(1:n))
+  
+  for (i in x){
+    print(i*5)
+  }  
 }
 
